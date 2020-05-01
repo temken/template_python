1205e1c494484533935a681e624d53a56dfeafbd
Timon Emken <emken@chalmers.se>
2020-05-01 23:54:56 +0200

    Adds argparse.

--- package_name/command_line.py
+++ package_name/command_line.py
@@ -1,30 +1,17 @@
-# import sys
-# import argparse
+import sys
+import argparse
 
 from .some_module import fib, reverse_string
 
-
 def main():
-    print(fib(10))
-    print(reverse_string("foo"))
+	args = parse_args(sys.argv[1:])
+	print(fib(args.number))
+	print(reverse_string(args.some_string))
 
-
-# def parse_args(args):
-# 	parser = argparse.ArgumentParser(description="python project template")
-# 	parser.add_argument("number", help = "Some integer. (Default: 10)",
-# 						nargs='?', type = int, default = 10)
-# 	parser.add_argument("some_string", help = "Some string. (Default: 'test')",
-# 						nargs='?', type = str, default = "test")
-# 	return parser.parse_args()
-
-# def main():
-# 	# parser = argparse.ArgumentParser(description="python project template")
-# 	# parser.add_argument("number", help = "Some integer. (Default: 10)",
-# 	# 					nargs='?', type = int, default = 10)
-# 	# parser.add_argument("some_string", help = "Some string. (Default: 'test')",
-# 	# 					nargs='?', type = str, default = "test")
-# 	# args = parser.parse_args()
-
-# 	args = parse_args(sys.argv[1:])
-# 	print(fib(args.number))
-# 	print(reverse_string(args.some_string))
+def parse_args(args):
+	parser = argparse.ArgumentParser(description="python project template")
+	parser.add_argument("number", help = "Some integer. (Default: 10)",
+						nargs='?', type = int, default = 10)
+	parser.add_argument("some_string", help = "Some string. (Default: 'foo')",
+						nargs='?', type = str, default = "foo")
+	return parser.parse_args(args)
--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
-from package_name.command_line import main
+from package_name.command_line import main, parse_args
 
 class TestCmd(TestCase):
-    def test_basic(self):
-        main()
+	def test_parser(self):
+		parser = parse_args(['11', 'word'])
+		self.assertEqual(parser.number,11)
+		self.assertEqual(parser.some_string,"word")
